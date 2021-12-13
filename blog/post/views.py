e561b07da48b84a8d968e587d25614a9d239faa7
Utkirbek <mutaliyevutkirbek@gmail.com>
2021-12-13 12:49:32 +0500

    About page added

--- blog/post/views.py
+++ blog/post/views.py
@@ -3,5 +3,6 @@ from django.views.generic import TemplateView
 
 class homePageView(TemplateView):
     template_name = 'home.html'
-
+class aboutPageView(TemplateView):
+    template_name = 'about.html'
 
