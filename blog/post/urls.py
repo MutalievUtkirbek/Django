e4300ee546be1e2c2d04d2c82e2a979f5f8d22de
Utkirbek <mutaliyevutkirbek@gmail.com>
2021-12-13 15:30:18 +0500

    posts model and template added

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homePageView, aboutPageView
+from .views import homePageView, aboutPageView,postPageView
 
 urlpatterns = [
-    path('', homePageView.as_view() , name="home"),
-    path('about', aboutPageView.as_view() , name="about")
+    path('', homePageView.as_view(), name="home"),
+    path('about', aboutPageView.as_view(), name="about"),
+    path('post', postPageView.as_view(), name="post"),
 ]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import Post
+
 
 class homePageView(TemplateView):
     template_name = 'home.html'
+
+
 class aboutPageView(TemplateView):
     template_name = 'about.html'
 
+
+class postPageView(ListView):
+    template_name = 'post.html'
+    model = Post
